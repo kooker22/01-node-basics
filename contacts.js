6921bcd1e1ea056caa09c5d784fce1f9c3d49822
Иван Попович <kooker22>
2020-09-15 19:03:07 +0300

    hw1

--- contacts.js
+++ contacts.js
@@ -6,7 +6,7 @@ module.exports.listContacts = async function () {
   const contacts = await fs
     .readFile(contactsPath, { encoding: 'utf-8' })
     .then((data) => JSON.parse(data));
-    console.table(contacts);
+  console.table(contacts);
 };
 
 module.exports.getContactById = async function (contactId) {
@@ -16,7 +16,7 @@ module.exports.getContactById = async function (contactId) {
       .then((data) => JSON.parse(data));
     const findedContact = contacts.find((contact) => contact.id === contactId);
     console.table(findedContact);
-    
+
     return findedContact;
   } catch (error) {
     console.log('error', error);
@@ -27,18 +27,12 @@ module.exports.removeContact = async function (contactId) {
   const contacts = await fs
     .readFile(contactsPath, { encoding: 'utf-8' })
     .then((data) => JSON.parse(data));
-  const filteredList = contacts.filter(
-    (contact) => contact.id !== contactId
-  );
+  const filteredList = contacts.filter((contact) => contact.id !== contactId);
   const FilteredListAsJSON = JSON.stringify(filteredList);
   console.table(filteredList);
   fs.writeFile(contactsPath, FilteredListAsJSON, (err) => {
-    
     if (err) throw err;
-     
-    
   });
-  
 };
 
 module.exports.addContact = async function (name, email, phone) {
@@ -59,7 +53,7 @@ module.exports.addContact = async function (name, email, phone) {
   contacts.push(newContact);
   const newContactListAsJSON = JSON.stringify(contacts);
   fs.writeFile(contactsPath, newContactListAsJSON);
-  console.table(contacts)
+  console.table(contacts);
 };
 
 // getContactById(2);
